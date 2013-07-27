96d73d7a0fca069de33967b1f992b63a5404b257
Dave Lin <gsldave510@gmail.com>
2013-07-27 11:08:29 +0800

    Generated profile controller

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -32,4 +32,13 @@ class UserTest < ActiveSupport::TestCase
 		assert !user.errors[:profile_name].empty?
 		assert user.errors[:profile_name].include?("Must be formatted correctly.")
 	end
+
+	test "a user can have a correctly formatted profile name" do
+		user = User.new(first_name: 'Xi', last_name: 'Lin', email: 'gsldave@hotmail.com')
+		user.password = user.password_confirmation = '12345678'
+
+		user.profile_name = 'gsldave510'
+		assert user.valid?
+	end
+
 end
